0ddbbd5c722d17a2137b9d8fc72981ada2f2acb9
sharanya <mallalasridhar007@gmail.com>
2022-02-03 17:58:57 +0530

    raj

---
+++ fractional/fractional/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace fractional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double value = 0;
+            int whole = 0;
+            double fractional = 0;
+            Console.WriteLine("enter the whole number");
+            int no = int.Parse(Console.ReadLine());
+            fractional = value - whole;
+            Console.WriteLine(whole );
+            Console.WriteLine( fractional );
+        }
+    }
+}
